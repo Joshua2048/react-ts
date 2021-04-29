2b186943482329bff563b40d3aed8a7aa067f7d7
Zhangzhenhua-web <15710046780@163.com>
2021-04-29 10:05:34 +0800

    task

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit1 <code>src/App.tsx</code> and save to reload.
+          Edit12 <code>src/App.tsx</code> and save to reload.
         </p>
 
         <a
